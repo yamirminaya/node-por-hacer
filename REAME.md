bc6091c21c8657dec39dac8f0901bbbc617d158e
Yamir <yamir@moopixel.com>
2020-10-04 15:27:55 -0500

    Primer commit

---
+++ REAME.md
@@ -0,0 +1,9 @@
+## Aplicación de comandos
+
+Ejercicio del curso
+
+Instalar paquetes
+
+```
+npm install
+```
---
+++ config/yargs.js
@@ -0,0 +1,29 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+}
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualizar el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borrar un elemento', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
